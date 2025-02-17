00fa15cf70738e4410a79790981658456989d53e
Ivonne Cantor <ivonnecp@gmail.com>
2025-02-17 21:42:01 +0100

    ejercicios hasta universidad parte 2 ok

--- nivel01.sql
+++ nivel01.sql
@@ -17,4 +17,21 @@ select asg.nombre as "Asignaturas del alumno con NIF 26902806M", ce.anyo_inicio,
 select distinct departamento.nombre from departamento  inner join profesor on departamento.id = profesor.id_departamento inner join asignatura on profesor.id_profesor = asignatura.id_profesor inner join grado on asignatura.id_grado = grado.id where grado.id = 4;
 -- 09
 select distinctrow persona.nombre, persona.apellido1 from persona inner join alumno_se_matricula_asignatura on persona.id = alumno_se_matricula_asignatura.id_alumno inner join curso_escolar on alumno_se_matricula_asignatura.id_curso_escolar=curso_escolar.id where curso_escolar.anyo_inicio = 2018 and curso_escolar.anyo_fin=2019; 
+-- LEFT JOIN i RIGHT JOIN.
 -- 10
+select pe.apellido1, pe.apellido2, pe.nombre, dep.nombre as departamento from persona as pe left join profesor as pro on pe.id = pro.id_profesor left join departamento as dep on pro.id_departamento=dep.id where pe.tipo="profesor" order by dep.nombre, pe.apellido1, pe.apellido2, pe.nombre;
+-- 11
+select pe.apellido1, pe.apellido2, pe.nombre, dep.nombre as departamento from persona as pe left join profesor as pro on pe.id = pro.id_profesor left join departamento as dep on pro.id_departamento=dep.id where pe.tipo="profesor" and dep.nombre is null order by dep.nombre, pe.apellido1, pe.apellido2, pe.nombre;
+-- 12
+select dep.nombre as departamento from persona as pe inner join profesor as pro on pe.id = pro.id_profesor right join departamento as dep on pro.id_departamento=dep.id where pe.nombre is null order by dep.nombre;
+-- 13
+select concat(pe.nombre, " ", pe.apellido1, " " , pe.apellido2) as Profesor from persona as pe left join profesor as pro on pe.id = pro.id_profesor left join asignatura as asg on pro.id_profesor = asg.id_profesor where pe.tipo = "profesor" and asg.nombre is null order by Profesor;
+-- 14
+select asg.nombre as "Asignaturas sin profesor asignado" from asignatura as asg left join profesor as pr on asg.id_profesor=pr.id_profesor where pr.id_profesor is null;
+-- 15
+select distinct dep.nombre from departamento as dep left join profesor as pro on dep.id = pro.id_departamento left join asignatura as asg on pro.id_profesor = asg.id_profesor left join alumno_se_matricula_asignatura as al on asg.id = al.id_asignatura where asg.nombre is null and al.id_curso_escolar is null;
+-- Consultas resumen
+-- 16
+
+
+
